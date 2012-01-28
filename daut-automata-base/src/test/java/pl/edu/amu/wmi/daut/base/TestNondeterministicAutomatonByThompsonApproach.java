ff0b9137f5cb3c6d05ad2b15f5ec5fdd1c089e02
as52048 <andq74@gmail.com>
2012-01-28 12:05:02 +0100

    Update daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestNondeterministicAutomatonByThompsonApproach.java

--- daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestNondeterministicAutomatonByThompsonApproach.java
+++ daut-automata-base/src/test/java/pl/edu/amu/wmi/daut/base/TestNondeterministicAutomatonByThompsonApproach.java
@@ -241,113 +241,106 @@ public class TestNondeterministicAutomatonByThompsonApproach extends TestCase {
 	*/
  
  
-    public final void testNondeterministicAutomatonWithEpsilonTransition(){
-        final AutomatonSpecification specHex = new NaiveAutomatonSpecification();
+	public final void testNondeterministicAutomatonWithEpsilonTransition(){
+		final AutomatonSpecification specHex = new NaiveAutomatonSpecification();
 
-        State q0 = specHex.addState();
-        State q1 = specHex.addState();
-        State q2 = specHex.addState();
-        State q3 = specHex.addState();
-        State q4 = specHex.addState();
-		
-	specHex.addTransition(q0, q1, new CharTransitionLabel('0'));
-        specHex.addTransition(q0, q1, new CharTransitionLabel('1'));
-        specHex.addTransition(q0, q1, new CharTransitionLabel('2'));
-        specHex.addTransition(q0, q1, new CharTransitionLabel('3'));
-        specHex.addTransition(q0, q1, new CharTransitionLabel('4'));
-        specHex.addTransition(q0, q1, new CharTransitionLabel('5'));
-	specHex.addTransition(q0, q1, new CharTransitionLabel('6'));
-        specHex.addTransition(q0, q1, new CharTransitionLabel('7'));
-        specHex.addTransition(q0, q1, new CharTransitionLabel('8'));
-        specHex.addTransition(q0, q1, new CharTransitionLabel('9'));
-	specHex.addTransition(q0, q1, new CharTransitionLabel('A'));
-        specHex.addTransition(q0, q1, new CharTransitionLabel('B'));
-        specHex.addTransition(q0, q1, new CharTransitionLabel('C'));
-        specHex.addTransition(q0, q1, new CharTransitionLabel('D'));
-        specHex.addTransition(q0, q1, new CharTransitionLabel('E'));
-	specHex.addTransition(q0, q1, new CharTransitionLabel('F'));
-		
-	specHex.addTransition(q1, q2, new CharTransitionLabel('0'));
-        specHex.addTransition(q1, q2, new CharTransitionLabel('1'));
-        specHex.addTransition(q1, q2, new CharTransitionLabel('2'));
-        specHex.addTransition(q1, q2, new CharTransitionLabel('3'));
-        specHex.addTransition(q1, q2, new CharTransitionLabel('4'));
-        specHex.addTransition(q1, q2, new CharTransitionLabel('5'));
-        specHex.addTransition(q1, q2, new CharTransitionLabel('6'));
-        specHex.addTransition(q1, q2, new CharTransitionLabel('7'));
-        specHex.addTransition(q1, q2, new CharTransitionLabel('8'));
-        specHex.addTransition(q1, q2, new CharTransitionLabel('9'));
-	specHex.addTransition(q1, q2, new CharTransitionLabel('A'));
-        specHex.addTransition(q1, q2, new CharTransitionLabel('B'));
-        specHex.addTransition(q1, q2, new CharTransitionLabel('C'));
-        specHex.addTransition(q1, q2, new CharTransitionLabel('D'));
-        specHex.addTransition(q1, q2, new CharTransitionLabel('E'));
-	specHex.addTransition(q1, q2, new CharTransitionLabel('F'));
-		
-	specHex.addTransition(q2, q3, new CharTransitionLabel('0'));
-        specHex.addTransition(q2, q3, new CharTransitionLabel('1'));
-        specHex.addTransition(q2, q3, new CharTransitionLabel('2'));
-        specHex.addTransition(q2, q3, new CharTransitionLabel('3'));
-        specHex.addTransition(q2, q3, new CharTransitionLabel('4'));
-        specHex.addTransition(q2, q3, new CharTransitionLabel('5'));
-        specHex.addTransition(q2, q3, new CharTransitionLabel('6'));
-        specHex.addTransition(q2, q3, new CharTransitionLabel('7'));
-        specHex.addTransition(q2, q3, new CharTransitionLabel('8'));
-        specHex.addTransition(q2, q3, new CharTransitionLabel('9'));
-	specHex.addTransition(q2, q3, new CharTransitionLabel('A'));
-        specHex.addTransition(q2, q3, new CharTransitionLabel('B'));
-        specHex.addTransition(q2, q3, new CharTransitionLabel('C'));
-        specHex.addTransition(q2, q3, new CharTransitionLabel('D'));
-        specHex.addTransition(q2, q3, new CharTransitionLabel('E'));
-	specHex.addTransition(q2, q3, new CharTransitionLabel('F'));
-		
-	specHex.addTransition(q3, q4, new CharTransitionLabel('0'));
-        specHex.addTransition(q3, q4, new CharTransitionLabel('1'));
-        specHex.addTransition(q3, q4, new CharTransitionLabel('2'));
-        specHex.addTransition(q3, q4, new CharTransitionLabel('3'));
-        specHex.addTransition(q3, q4, new CharTransitionLabel('4'));
-        specHex.addTransition(q3, q4, new CharTransitionLabel('5'));
-	specHex.addTransition(q3, q4, new CharTransitionLabel('6'));
-        specHex.addTransition(q3, q4, new CharTransitionLabel('7'));
-        specHex.addTransition(q3, q4, new CharTransitionLabel('8'));
-        specHex.addTransition(q3, q4, new CharTransitionLabel('9'));
-	specHex.addTransition(q3, q4, new CharTransitionLabel('A'));
-        specHex.addTransition(q3, q4, new CharTransitionLabel('B'));
-        specHex.addTransition(q3, q4, new CharTransitionLabel('C'));
-        specHex.addTransition(q3, q4, new CharTransitionLabel('D'));
-        specHex.addTransition(q3, q4, new CharTransitionLabel('E'));
-	specHex.addTransition(q3, q4, new CharTransitionLabel('F'));
-		
-	specHex.addTransition(q4, q0, new EpsilonTransitionLabel());
-		
-	specHex.addTransition(q4, q1, new CharTransitionLabel('a'));
+		State q0 = specHex.addState();
+		State q1 = specHex.addState();
+		State q2 = specHex.addState();
+		State q3 = specHex.addState();
+		State q4 = specHex.addState();
 
-        
+		specHex.addTransition(q0, q1, new CharTransitionLabel('0'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('1'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('2'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('3'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('4'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('5'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('6'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('7'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('8'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('9'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('A'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('B'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('C'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('D'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('E'));
+		specHex.addTransition(q0, q1, new CharTransitionLabel('F'));
 
-        specHex.markAsInitial(q0);
-        specHex.markAsFinal(q4);
+		specHex.addTransition(q1, q2, new CharTransitionLabel('0'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('1'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('2'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('3'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('4'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('5'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('6'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('7'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('8'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('9'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('A'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('B'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('C'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('D'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('E'));
+		specHex.addTransition(q1, q2, new CharTransitionLabel('F'));
 
-        final NondeterministicAutomatonByThompsonApproach automat0F = 
-		new NondeterministicAutomatonByThompsonApproach(specHex);
+		specHex.addTransition(q2, q3, new CharTransitionLabel('0'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('1'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('2'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('3'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('4'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('5'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('6'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('7'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('8'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('9'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('A'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('B'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('C'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('D'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('E'));
+		specHex.addTransition(q2, q3, new CharTransitionLabel('F'));
 
-        assertTrue(automat0F.accepts("00007777AAAAFFFF"));
-        assertTrue(automat0F.accepts("FFFF00003333"));
-        assertTrue(automat0F.accepts("00fa"));
-        assertTrue(automat0F.accepts("99991111"));
-        assertTrue(automat0F.accepts("aaaa"));
-        assertTrue(automat0F.accepts("FF00"));
-        assertTrue(automat0F.accepts("F0F0"));
-        assertTrue(automat0F.accepts("0F0F"));
-        assertTrue(automat0F.accepts("F55A"));
-        assertTrue(automat0F.accepts("abCF"));
-        assertTrue(automat0F.accepts("000F"));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('0'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('1'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('2'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('3'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('4'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('5'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('6'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('7'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('8'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('9'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('A'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('B'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('C'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('D'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('E'));
+		specHex.addTransition(q3, q4, new CharTransitionLabel('F'));
+	
+		specHex.addTransition(q4, q0, new EpsilonTransitionLabel());
 
-        assertFalse(automat0F.accepts("LN61R"));
-        assertFalse(automat0F.accepts("T"));
-        assertFalse(automat0F.accepts("00F"));
-        assertFalse(automat0F.accepts("2,10"));
-        assertFalse(automat0F.accepts(""));
-        assertFalse(automat0F.accepts("\n"));
-        assertFalse(automat0F.accepts("true"));
+		specHex.addTransition(q4, q1, new CharTransitionLabel('0'));
+
+
+
+		specHex.markAsInitial(q0);
+		specHex.markAsFinal(q4);
+
+		final NondeterministicAutomatonByThompsonApproach automat0F = 
+			new NondeterministicAutomatonByThompsonApproach(specHex);
+
+		assertTrue(automat0F.accepts("00007777AAAAFFFF"));
+		assertTrue(automat0F.accepts("FFFF00003333"));
+		assertTrue(automat0F.accepts("99991111"));
+		assertTrue(automat0F.accepts("ABCD"));
+		assertTrue(automat0F.accepts("000F"));
+
+		assertFalse(automat0F.accepts("LN61R"));
+		assertFalse(automat0F.accepts("T"));
+		assertFalse(automat0F.accepts("2,10"));
+		assertFalse(automat0F.accepts(""));
+		assertFalse(automat0F.accepts("\n"));
+		assertFalse(automat0F.accepts("true"));
     }
 }
